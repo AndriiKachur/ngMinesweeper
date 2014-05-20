ed78f62a3a86316f52451e68b5a90b6aee4332fc
Nilanno <aa.kachur@gmail.com>
2014-05-20 22:01:08 +0300

    playable game v.1

--- script.js
+++ script.js
@@ -2,35 +2,64 @@ angular.module('ngMinesweeper', [])
     .controller('MainCtrl', function($scope, Game, C) {
 
         Game.resetGame();
-
         $scope.game = Game;
         $scope.board = Game.board;
         $scope.message = '';
 
-        $scope.className = function(field) {
-            var className = '';
-            if (field.flag) {
-                className += ' flagged ';
+        function checkEndGame() {
+            if (Game.isGameEnded()) {
+                $scope.message = 'Nice work!';
+                Game.stopped = true;
             }
-            return className;
-        };
+        }
 
-        $scope.toggleField = function(f) {
+        $scope.toggleField = function(f, x, y) {
+            var processed = [];
+            function openField(f, x, y) {
+                if (x >= 0 && x < Game.side && y >= 0 && y < Game.side) {
+
+                    if (f === null) {
+                        f = Game.board.rows[x][y];
+                        if (f.processed) return;
+                    }
+
+                    f.open = true;
+                    f.processed = true;
+                    processed.push(f);
+                    if (f.value === 0) {
+                        openField(null, x - 1, y - 1);
+                        openField(null, x - 1, y);
+                        openField(null, x - 1, y + 1);
+                        openField(null, x + 1, y - 1);
+                        openField(null, x + 1, y);
+                        openField(null, x + 1, y + 1);
+                        openField(null, x, y + 1);
+                        openField(null, x, y - 1);
+                    }
+                }
+            }
+
             if (Game.stopped) return;
             if (f.flag) return;
 
             if (f.value === C.mine) {
                 Game.stopped = true;
                 $scope.message = 'Oooops, minesweeper has only one chance for mistake...';
+                f.open = true;
+                return;
             }
-
-            f.open = true;
+            openField(f, x, y);
+            angular.forEach(processed, function(field) {
+                delete field.processed;
+            });
+            checkEndGame();
         };
         $scope.markField = function(f) {
             if (Game.stopped) return;
             if (f.open) return;
 
             f.flag = !f.flag;
+             checkEndGame();
         };
 
 
@@ -55,10 +84,22 @@ angular.module('ngMinesweeper', [])
             mines: 10,
             board: null,
             stopped: false,
+            minesCache: [],
+
+            isGameEnded: function() {
+                var hasNotFoundMines = Array.prototype.some.call(game.minesCache, function(mineField) {
+                        if (!mineField.flag) {
+                            return true;
+                        }
+                    });
+                return !game.stopped && !hasNotFoundMines;
+            },
 
             resetGame: function () {
+                game.stopped = false;
                 game.board = {};
                 game.board.rows = [];
+                game.minesCache = [];
                 for (var i = 0, row; i < game.side; ++i) {
                     row = [];
                     for (var j = 0; j < game.side; ++j) {
@@ -72,12 +113,14 @@ angular.module('ngMinesweeper', [])
 
                 (function insertMinesAndValues(){
                     function insertMine() {
-                        var x = parseInt(Math.random() * game.side),
-                            y = parseInt(Math.random() * game.side);
-                        if (game.board.rows[x][y].value) {
+                        var x = Math.random() * game.side | 0,
+                            y = Math.random() * game.side | 0,
+                            field = game.board.rows[x][y];
+                        if (field.value) {
                             return;
                         }
-                        game.board.rows[x][y].value = C.mine;
+                        field.value = C.mine;
+                        game.minesCache.push(field);
                         return true;
                     }
                     function isMined(x, y) {
@@ -85,7 +128,7 @@ angular.module('ngMinesweeper', [])
                         return r[x] && r[x][y] && (r[x][y].value === C.mine);
                     }
 
-                    for ( var i = 0; i < game.mines; ++i) {
+                    for (var i = 0; i < game.mines; ++i) {
                         var mineInserted = false;
                         while (!mineInserted) {
                             mineInserted = insertMine();
@@ -95,9 +138,8 @@ angular.module('ngMinesweeper', [])
                     for (var i = 0; i < game.side; ++i) {
                         for (var j = 0; j < game.side; ++j) {
                             var field = game.board.rows[i][j],
-                                rows = game.board.rows,
                                 minesAround = 0;
-                            if (field.value === C.mine) return;
+                            if (field.value === C.mine) continue;
 
                             if (isMined(i - 1, j - 1)) ++minesAround;
                             if (isMined(i - 1, j)) ++minesAround;
@@ -111,10 +153,9 @@ angular.module('ngMinesweeper', [])
                             field.value = minesAround;
                         }
                     }
-
                 })();
-
             }
+
         };
 
         return game;
